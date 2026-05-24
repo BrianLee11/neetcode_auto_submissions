b8def36b96964e0e5aa57ec648c7627e21ec254b
Brian J. Lee <134612285+BrianLee11@users.noreply.github.com>
2026-05-24 10:22:03 -0400

    Add: python-return-statement - submission-0

---
+++ Python For Beginners/python-return-statement/submission-0.py
@@ -0,0 +1,10 @@
+def product(number1, number2):
+    return number1 * number2
+
+
+
+# don't modify below this line
+print(product(2, 4))
+print(product(8, 2))
+print(product(4, 8))
+print(product(8, 8))
