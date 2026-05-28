c7d439cbf6af296efa656b3aec5bb973eb33a9c2
Brian J. Lee <134612285+BrianLee11@users.noreply.github.com>
2026-05-28 17:32:15 -0400

    Add: sql-default-values - submission-2

---
+++ SQL/sql-default-values/submission-2.sql
@@ -0,0 +1,19 @@
+/* Relational databases store data in tables.
+A table consists of rows and columns.
+*/
+
+CREATE TABLE videos(
+    id INTEGER,
+    name TEXT,
+    created_AT DATE,
+    published BOOLEAN
+/*  This is the schema of the table.
+ It's analogous to a class definition in
+ object-oriented programming.
+ */
+);
+
+-- Do not modify below this line --
+SELECT table_name, column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'videos';
