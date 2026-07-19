cf137d2552b6b24d82c455ce79f1d5abd174baf3
Brian J. Lee <134612285+BrianLee11@users.noreply.github.com>
2026-07-19 08:35:26 -0400

    Add: max-consecutive-ones - submission-2

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-2.java
@@ -0,0 +1,18 @@
+public class Solution {
+        public int findMaxConsecutiveOnes(int[] nums)
+    {        
+        int current_occurrence_1 = 0;
+        int max_occurrence_1 = 0;
+
+        for (int number : nums)
+        {
+            if (number == 0){ current_occurrence_1 = 0;}
+            else { current_occurrence_1 += 1;}
+
+            if (max_occurrence_1 < current_occurrence_1) 
+                {max_occurrence_1 = current_occurrence_1;}
+        }
+
+        return max_occurrence_1;
+    }
+}
